252932119082f0fccaafc3ce2f7592e4c7dd8c67
lcarole <luigi.carole@gmail.com>
2025-03-03 02:15:26 +0100

    Ajout du support du fichier ".env" dans le fichier "database.py".

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 from sqlmodel import create_engine, Session
 
+load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:postgres@localhost/blog_db")
+
+DATABASE_URL = os.getenv("DATABASE_URL","postgresql://postgres:postgres@localhost/blo")
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
